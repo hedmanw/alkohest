4bfa36f29741caae6c5b9eda8f99b0c4c0c54606
Wilhelm Hedman <w.hedman@gmail.com>
2015-06-27 22:14:00 +0200

    Saves course selection

---
+++ src/clients/StorageClient.jsx
@@ -0,0 +1,41 @@
+import PubSub from "pubsub-js"
+
+class StorageClient {
+
+    setPinned(id) {
+        if (window.localStorage) {
+            let pinned = this.getPinned();
+            if (pinned.length == 0) {
+                window.localStorage.setItem('pinned', JSON.stringify([id]));
+            }
+            else {
+                let head = pinned[0];
+                window.localStorage.setItem('pinned', JSON.stringify([id, head]));
+            }
+            PubSub.publish('pin')
+        }
+    }
+
+    isPinned(id) {
+        return this.getPinned().filter(item => item === id).length > 0;
+    }
+
+    getPinned() {
+        if (window.localStorage && window.localStorage.getItem('pinned')) {
+            return JSON.parse(window.localStorage.getItem('pinned'));
+        }
+        else {
+            return [];
+        }
+    }
+
+    subscribe(callback) {
+        return PubSub.subscribe('pin', callback);
+    }
+
+    unsubscribe(token) {
+        PubSub.unsubscribe(token);
+    }
+}
+
+export default new StorageClient();
--- src/components/CourseList.jsx
+++ src/components/CourseList.jsx
@@ -1,12 +1,12 @@
 import React from "react";
 import Card from "./Card.jsx";
-
+import StorageClient from "../clients/StorageClient.jsx"
 
 let CourseList = React.createClass({
     render() {
         let courses = this.props.data.map(item => {
             return (
-                <CourseRow code={item.courseCode} homepage={item.courseUrl} fire={item.fireUrl}>{item.courseName}</CourseRow>
+                <CourseRow id={item.id} code={item.courseCode} homepage={item.courseUrl} fire={item.fireUrl}>{item.courseName}</CourseRow>
             )
         });
         return(
@@ -29,21 +29,49 @@ let CourseList = React.createClass({
 });
 
 let CourseRow = React.createClass({
+    getInitialState() {
+        return {
+            pinned: StorageClient.isPinned(this.props.id),
+            token: null
+        };
+    },
+    componentDidMount() {
+        let token = StorageClient.subscribe(function() {
+            this.setState({pinned: StorageClient.isPinned(this.props.id)});
+        }.bind(this));
+        this.setState({token: token});
+    },
+    componentWillUnmount() {
+        if (this.state.token) {
+            StorageClient.unsubscribe(this.state.token);
+        }
+    },
+    handleClick() {
+        StorageClient.setPinned(this.props.id)
+    },
     render() {
         let fire;
         if (typeof this.props.fire !== 'undefined') {
             fire = <td><a href={this.props.fire}>Fire</a></td>;
         }
         else {
-            fire = <td>N/A</td>
+            fire = <td></td>
         }
+        let pinButton;
+        if (this.state.pinned) {
+            pinButton = <a className="btn disabled"><i className="material-icons">done</i></a>
+        }
+        else {
+            pinButton = <a onClick={this.handleClick} className="btn blue"><i className="material-icons">add</i></a>
+        }
+
         return(
             <tr>
                 <td>{this.props.code}</td>
                 <td>{this.props.children}</td>
                 <td><a href={this.props.homepage}>Hemsida</a></td>
                 {fire}
-                <th><a href="#"><i className="material-icons">send</i></a></th>
+                <td>{pinButton}</td>
             </tr>
         )
     }
