ca9df052ffdaf462840b7360ec04b68a5b0c8b0e
Wilhelm Hedman <w.hedman@gmail.com>
2015-07-02 20:10:38 +0200

    Disposition

--- src/components/admin/AdminPanel.jsx
+++ src/components/admin/AdminPanel.jsx
@@ -22,9 +22,7 @@ let AdminPanel = React.createClass({
                         <AdminCourseList/>
                     </div>
                 </div>
-
             </div>
-
         );
     }
 });
--- src/components/admin/CourseEditor.jsx
+++ src/components/admin/CourseEditor.jsx
@@ -5,11 +5,15 @@ let CourseEditor = React.createClass({
         return (
             <div className="row">
                 <div className="row">
+                    <div className="input-field col m1">
+                        <input disabled value="1" id="dbId" type="text" class="validate"/>
+                        <label for="dbId">Database id</label>
+                    </div>
                     <div className="input-field col m4">
                         <input id="courseCode" type="text" className="validate"/>
                         <label for="courseCode">Course code</label>
                     </div>
-                    <div className="input-field col m8">
+                    <div className="input-field col m7">
                         <input id="courseName" type="text" className="validate"/>
                         <label for="courseName">Course name</label>
                     </div>
@@ -19,15 +23,15 @@ let CourseEditor = React.createClass({
                         <input id="courseUrl" type="url" className="validate"/>
                         <label for="courseUrl">Course homepage</label>
                     </div>
-                    <div className="input-field col m6">
-                        <input id="fireUrl" type="url" className="validate"/>
-                        <label for="fireUrl">Fire url</label>
+                    <div className="input-field col m3">
+                        <input disabled value="asdfas" id="lastAccessed" type="text" class="validate"/>
+                        <label for="lastAccessed">Last added</label>
                     </div>
                 </div>
                 <div className="row">
-                    <div className="input-field col m3">
-                        <input disabled value="asdfas" id="lastAccessed" type="text" class="validate"/>
-                        <label for="lastAccessed">Last accessed</label>
+                    <div className="input-field col m6">
+                        <input id="fireUrl" type="url" className="validate"/>
+                        <label for="fireUrl">Fire url</label>
                     </div>
                     <div className="input-field col m3">
                         <input disabled value="asdfas" id="timesAdded" type="text" class="validate"/>
