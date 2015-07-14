9e31c6dc12dfd53a03e995d1cb30f3792f74e4ba
Wilhelm Hedman <w.hedman@gmail.com>
2015-07-14 20:53:22 +0200

    Stop messing with me, react

--- src/components/admin/CourseEditor.jsx
+++ src/components/admin/CourseEditor.jsx
@@ -5,7 +5,11 @@ import CourseClient from "../../clients/CourseClient.js";
 let CourseEditor = React.createClass({
     getInitialState() {
         return {
-            response: null
+            response: null,
+            courseCode: this.props.course.courseCode,
+            courseName: this.props.course.courseName,
+            courseUrl: this.props.course.courseUrl,
+            fireUrl: this.props.course.fireUrl
         };
     },
     approve() {
@@ -19,7 +23,7 @@ let CourseEditor = React.createClass({
         // The callback should really be registered one level up so this component can be redrawn/redirected/whatever accordingly?
         // check out react router if a "redirect" is in order.
         if (this.props.course.id) {
-            CourseClient.editCourse(course.id, data, (data) => {
+            CourseClient.editCourse(this.props.course.id, data, (data) => {
                 this.setState({response: data})
             }, (err) => {
                 this.setState({response: err})
@@ -41,41 +45,46 @@ let CourseEditor = React.createClass({
             // TODO: Link to back
         }
     },
+    changeCode(event) {
+        this.setState({courseCode: event.target.value});
+    },
     render() {
         return (
-            <div className="row">
+            <div>
                 <div className="row">
                     <div className="input-field col m2">
                         <input disabled id="dbId" type="text" className="validate" value={this.props.course.id}/>
-                        <label htmlFor="dbId">ID</label>
+                        <label className="active" htmlFor="dbId">ID</label>
                     </div>
                     <div className="input-field col m4">
-                        <input id="courseCode" ref="courseCode" type="text" className="validate" value={this.props.course.courseCode}/>
-                        <label htmlFor="courseCode">Course code</label>
+                        <input id="courseCode" ref="courseCode" type="text" className="validate" value={this.state.courseCode} onChange={this.changeCode}/>
+                        <label className="active" htmlFor="courseCode">Course code</label>
                     </div>
                 </div>
                 <div className="row">
                     <div className="input-field col m6">
-                        <input id="courseName" ref="courseName" type="text" className="validate" value={this.props.course.courseName}/>
-                        <label htmlFor="courseName">Course name</label>
+                        <input id="courseName" ref="courseName" type="text" className="validate" value={this.state.courseName}/>
+                        <label className="active" htmlFor="courseName">Course name</label>
                     </div>
                 </div>
                 <div className="row">
                     <div className="input-field col m6">
-                        <input id="courseUrl" ref="courseUrl" type="url" className="validate" value={this.props.course.courseUrl}/>
-                        <label htmlFor="courseUrl">Course homepage</label>
+                        <input id="courseUrl" ref="courseUrl" type="url" className="validate" value={this.state.courseUrl}/>
+                        <label className="active" htmlFor="courseUrl">Course homepage</label>
                     </div>
                 </div>
                 <div className="row">
                     <div className="input-field col m6">
-                        <input id="fireUrl" ref="fireUrl" type="url" className="validate" value={this.props.course.fireUrl}/>
-                        <label htmlFor="fireUrl">Fire url</label>
+                        <input id="fireUrl" ref="fireUrl" type="url" className="validate" value={this.state.fireUrl}/>
+                        <label className="active" htmlFor="fireUrl">Fire url</label>
                     </div>
                 </div>
                 <div className="row">
-                    <a onClick={this.approve} className="btn green">Skapa</a>
-                    <a onClick={this.delete} className="btn red">Släng</a>
-                    <p>{this.state.response}</p>
+                        <div className="col m6">
+                        <a onClick={this.approve} className="btn green">Skapa</a>
+                        <a onClick={this.delete} className="btn red">Släng</a>
+                        <p>{this.state.response}</p>
+                    </div>
                 </div>
             </div>
         );
--- src/components/admin/EditPanel.jsx
+++ src/components/admin/EditPanel.jsx
@@ -1,19 +1,26 @@
 import React from "react";
 import CourseEditor from "./CourseEditor.jsx";
+import CourseClient from "../../clients/CourseClient.js";
 
 let EditPanel = React.createClass({
     getInitialState() {
-        let {courseID} = this.props.params;
         return {
-            course: courseID
+            course: {}
         };
+    },
+
+    componentWillMount() {
+        let {courseID} = this.props.params;
+        CourseClient.getById(courseID, (data) => {
+            this.setState({course: data})
+        }, () => {});
     },
 
     render() {
         return (
             <div>
-                <h1 className="header purple-text">Ändra på gammal makapär {this.state.course}</h1>
-                <CourseEditor course={{}}/>
+                <h1 className="header purple-text">Ändra på gammal makapär</h1>
+                <CourseEditor course={this.state.course}/>
             </div>
         );
     }
