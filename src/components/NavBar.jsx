249500e5a6055e5537e1313334b0d0edede38f10
Wilhelm Hedman <hedmanw@student.chalmers.se>
2016-01-14 15:18:55 +0100

    Material design icons are made for trolling

--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -4,19 +4,21 @@ import {Link, State} from "react-router";
 let NavBar = React.createClass({
     render() {
         return (
-        <nav className="grey darken-4" role="navigation">
-            <div className="nav-wrapper container"><Link to="app" className="brand-logo navbar-brand">Alkohest</Link>
-                <ul className="right hide-on-med-and-down">
-                    <NavTab to="select">Alla kurser</NavTab>
-                </ul>
+            <nav className="grey darken-4" role="navigation">
+                <div className="nav-wrapper container">
+                    <Link to="app" className="brand-logo navbar-brand">Alkohest</Link>
+                    <ul className="right hide-on-med-and-down">
+                        <li><a className="white-text" href="http://git.alkohest.se" alt="Alkogit"><i className="material-icons">hot_tub</i></a></li>
+                        <NavTab to="select">Alla kurser</NavTab>
+                    </ul>
 
-                <ul id="nav-mobile" className="side-nav">
-                    <Link to="app">Framsidan</Link>
-                    <NavTab to="select">Alla kurser</NavTab>
-                </ul>
-                <a href="#" data-activates="nav-mobile" className="button-collapse"><i className="material-icons">menu</i></a>
-            </div>
-        </nav>
+                    <ul id="nav-mobile" className="side-nav">
+                        <Link to="app">Framsidan</Link>
+                        <NavTab to="select">Alla kurser</NavTab>
+                    </ul>
+                    <a href="#" data-activates="nav-mobile" className="button-collapse"><i className="material-icons">menu</i></a>
+                </div>
+            </nav>
         );
     }
 });
