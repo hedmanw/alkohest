7699849934ffb33f33ad60d5ab8fb7c17090d823
Wilhelm Hedman <w.hedman@gmail.com>
2016-02-14 21:10:52 +0100

    Sort courses by id or something, I don't know

--- server/course.js
+++ server/course.js
@@ -34,7 +34,9 @@ class CourseData {
     }
 
     getAll(res) {
-        Course.findAll().then(res);
+        Course.findAll({
+            order: 'id ASC'
+        }).then(res);
     }
     getById(id, res) {
         Course.findOne({
